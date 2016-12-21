da8a0e4eb378604e5de3ed181926e340ebec4654
rainzl <369857686@qq.com>
2016-12-22 00:44:25 +0800

    03

---
+++ js/menu.js
@@ -0,0 +1,32 @@
+$(function(){
+	var $menu = $('#menu');
+	//获取屏幕宽高
+	
+	
+	
+	
+	//设置菜单遮罩的宽高
+	setClient();
+	$(window).on('resize',setClient);
+	function setClient() {
+		var $H = window.innerHeight,
+			$W = window.innerWidth;
+		$menu.find('.mark').css({'width':$W,'height':$H});
+	}
+	//点击菜单按钮，显示菜单
+	$menu.find('.btn').off('click').on('click',function(){
+		if ( !this.onOff ) {
+			$menu.addClass('active');
+		} else {
+			var $menuList = $menu.find('.menuList'); 
+			$menuList.addClass('close');
+			var timer = setTimeout(function(){
+				$menu.removeClass('active');
+				$menuList.removeClass('close');
+			},200)
+			
+		}
+		this.onOff = !this.onOff;
+	});
+	
+})
